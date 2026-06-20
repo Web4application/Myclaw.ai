aaedbe8289f0253e4375f7fad18ba50ba982a72f
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-06-20 14:29:02 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -1,2 +1,5 @@
 curl -fsSL https://openclaw.ai/install.sh | bash
 powershell -c "& ([scriptblock]::Create((irm https://openclaw.ai/install.ps1))) -InstallMethod git"
+npm install -g openclaw@latest
+openclaw onboard --install-daemon
+openclaw dashboard
